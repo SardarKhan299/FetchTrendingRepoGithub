ff0651fe7f5a6aabc0110939f338b8f0f88bdc4b
SardarkhanStella <skhan@stellatechnology.com>
2023-06-27 18:59:06 +0500

    Add test class for FetchTrending Repo Usecase and make the test fail.

--- app/src/test/java/com/traiden/fetchtrendingrepo/data/repositories/FetchTrendingRepoUseCaseTest.kt
+++ app/src/test/java/com/traiden/fetchtrendingrepo/data/repositories/FetchTrendingRepoUseCaseTest.kt
@@ -3,7 +3,6 @@ package com.traiden.fetchtrendingrepo.data.repositories
 import com.traiden.fetchtrendingrepo.domain.Repository
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -12,35 +11,32 @@ import org.mockito.Mockito
 import org.mockito.junit.MockitoJUnitRunner
 
 @RunWith(MockitoJUnitRunner::class)
-class GithubRepositoryTest {
-
-
+class FetchTrendingRepoUseCaseTest {
     @Mock
-    private lateinit var apiService : GithubApiService
     private lateinit var githubRepository: GithubRepository
 
+    private lateinit var getTrendingRepositoriesUseCase: GetTrendingRepositoriesUseCase
+
     @Before
-    fun setup(){
-        githubRepository = GithubRepositoryImpl(apiService)
+    fun setup() {
+        getTrendingRepositoriesUseCase = GetTrendingRepositoriesUseCaseImpl(githubRepository)
     }
 
     @Test
-    fun fetchTrendingRepositories_should_return_list_of_repositories() {
+    fun `execute should return list of repositories`() {
         runBlocking {
             // Given
             val repositories = listOf(
                 Repository("Repo 1", "Owner 1", "Description 1", "", "java", 4),
                 Repository("Repo 2", "Owner 2", "Description 2", "", "kotlin", 3)
             )
-            Mockito.`when`(apiService.getTrendingRepositories()).thenReturn(repositories)
+            Mockito.`when`(githubRepository.getTrendingRepositories()).thenReturn(repositories)
 
             // When
-            val result = githubRepository.getTrendingRepositories()
+            val result = getTrendingRepositoriesUseCase.execute()
 
             // Then
             assertThat(result).isEqualTo(repositories)
         }
     }
-
-
 }
