222b21d512488fb1f8d25debaa50b99f99e7ba98
SardarkhanStella <skhan@stellatechnology.com>
2023-07-03 18:17:59 +0500

    Add Item view for repository recylerview and also add other required attributes.

--- app/src/main/java/com/traiden/fetchtrendingrepo/MainActivity.kt
+++ app/src/main/java/com/traiden/fetchtrendingrepo/MainActivity.kt
@@ -2,10 +2,21 @@ package com.traiden.fetchtrendingrepo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.traiden.fetchtrendingrepo.presentation.viewmodel.TrendingRepositoriesViewModel
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel: TrendingRepositoriesViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        observeViewModel()
+        viewModel.fetchTrendingRepositories()
+    }
+
+    private fun observeViewModel() {
+        viewModel.repositories.observe(this) { repositories ->
+            // Update the UI with the fetched repositories
+        }
     }
 }
