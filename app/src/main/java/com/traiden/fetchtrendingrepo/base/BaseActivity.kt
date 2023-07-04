4f0f2c992fa1a48963982a92023e8118623de471
SardarkhanStella <skhan@stellatechnology.com>
2023-07-04 17:16:40 +0500

    Add DataBinding Support To Project.

--- app/src/main/java/com/traiden/fetchtrendingrepo/base/BaseActivity.kt
+++ app/src/main/java/com/traiden/fetchtrendingrepo/base/BaseActivity.kt
@@ -4,17 +4,36 @@ import android.content.pm.ActivityInfo
 import android.content.res.Configuration
 import android.os.Bundle
 import android.util.Log
+import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import com.traiden.fetchtrendingrepo.R
 
-abstract class BaseActivity : AppCompatActivity() {
+abstract class BaseActivity(@LayoutRes private val layoutResId : Int) : AppCompatActivity() {
 
     protected var mPrevConfig: Configuration? = null
+    // Binding object
+    private lateinit var binding: ViewDataBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(BaseActivity::class.simpleName, "onCreate: ")
         mPrevConfig = Configuration(resources.configuration)
+        // Inflate the layout using data binding
+        binding = DataBindingUtil.setContentView(this, layoutResId)
+
+        // Perform additional setup or initialization
+        setupViews()
+    }
+
+    // Abstract method to be implemented by subclasses
+    abstract fun setupViews()
+
+    // Get the binding object
+    fun getBinding(): ViewDataBinding {
+        return binding
     }
 
 
--- app/src/main/java/com/traiden/fetchtrendingrepo/presentation/repository/MainActivity.kt
+++ app/src/main/java/com/traiden/fetchtrendingrepo/presentation/repository/MainActivity.kt
@@ -2,6 +2,7 @@ package com.traiden.fetchtrendingrepo.presentation.repository
 
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -9,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.facebook.shimmer.ShimmerFrameLayout
 import com.traiden.fetchtrendingrepo.R
 import com.traiden.fetchtrendingrepo.base.BaseActivity
+import com.traiden.fetchtrendingrepo.databinding.ActivityMainBinding
 import com.traiden.fetchtrendingrepo.domain.Items
 import com.traiden.fetchtrendingrepo.domain.NetworkResult
 import com.traiden.fetchtrendingrepo.presentation.viewmodel.TrendingRepositoriesViewModel
@@ -16,7 +18,7 @@ import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity(R.layout.activity_main) {
     private val viewModel: TrendingRepositoriesViewModel by viewModels()
 
     private lateinit var adapter: RepoAdapter
@@ -26,13 +28,19 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        recylerview = findViewById(R.id.rv_repositories)
-        shimmerFrameLayout = findViewById(R.id.shimmerLayout)
+        val binding: ActivityMainBinding = getBinding() as ActivityMainBinding
+        recylerview = binding.rvRepositories
+        shimmerFrameLayout = binding.shimmerLayout
+
         val llm = LinearLayoutManager(this)
         llm.orientation = LinearLayoutManager.VERTICAL
         recylerview.layoutManager = llm
         observeViewModel()
         viewModel.fetchTrendingRepositories()
+    }
+
+    override fun setupViews() {
+        Log.d(MainActivity::class.simpleName, "setupViews: ")
     }
 
     private fun observeViewModel() {
