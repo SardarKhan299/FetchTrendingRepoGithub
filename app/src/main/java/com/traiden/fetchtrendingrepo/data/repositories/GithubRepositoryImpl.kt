35bf9084b294b88beaf4af664838b6d93b2cdb23
SardarkhanStella <skhan@stellatechnology.com>
2023-07-05 14:03:14 +0500

    Refactoring of query string for api.

--- app/src/main/java/com/traiden/fetchtrendingrepo/data/repositories/GithubRepositoryImpl.kt
+++ app/src/main/java/com/traiden/fetchtrendingrepo/data/repositories/GithubRepositoryImpl.kt
@@ -3,11 +3,12 @@ package com.traiden.fetchtrendingrepo.data.repositories
 import com.traiden.fetchtrendingrepo.domain.GithubRepository
 import com.traiden.fetchtrendingrepo.domain.Items
 import com.traiden.fetchtrendingrepo.domain.Repository
+import com.traiden.fetchtrendingrepo.util.ApplicationConstants.Companion.API_QUERY
 import retrofit2.Response
 
 class GithubRepositoryImpl (private val apiService: ApiService) : GithubRepository {
     override suspend fun getTrendingRepositories(): Items {
         // Call the API service to fetch trending repositories
-        return apiService.getTrendingRepositories("language=+sort:stars")
+        return apiService.getTrendingRepositories(API_QUERY)
     }
 }
--- app/src/main/java/com/traiden/fetchtrendingrepo/util/ApplicationConstant.kt
+++ app/src/main/java/com/traiden/fetchtrendingrepo/util/ApplicationConstant.kt
@@ -3,5 +3,6 @@ package com.traiden.fetchtrendingrepo.util
 class ApplicationConstants {
     companion object {
         const val APP_THEME: String = "APP_THEME"
+        const val API_QUERY:String = "language=+sort:stars"
     }
 }
